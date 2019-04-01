149ea8473bde397d24a0760896265a54ea89f322
Satoshi Chibana Ota <satoshich567@gmail.com>
2019-03-31 21:37:27 -0400

    added company lead methods

--- app/controllers/company_leads_controller.rb
+++ app/controllers/company_leads_controller.rb
@@ -1,5 +1,7 @@
 class CompanyLeadsController < ApplicationController
 
+    before_action :find_lead, only: [:edit, :update, :show, :destroy]
+
     def index
         @company_leads = CompanyLead.all
     end
@@ -9,7 +11,7 @@ class CompanyLeadsController < ApplicationController
     end
 
     def create
-        @company_lead = CompanyLead.new(companyLead_params)
+        @company_lead = CompanyLead.new(company_lead_params)
         if @company_lead.save
             flash[:success] = "Register successful!"
             redirect_to company_leads_path
@@ -18,9 +20,36 @@ class CompanyLeadsController < ApplicationController
         end
     end
 
+    def edit
+
+    end
+
+    def update
+        if @company_lead.update(company_lead_params)
+            flash[:success] = "Lead updated successfully!"
+            redirect_to company_leads_path
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @company_lead.destroy
+        flash[:danger] = "Lead was succesfully deleted"
+        redirect_to company_leads_path
+    end
+
+    def show
+
+    end
+
     private
 
-    def companyLead_params
+    def find_lead
+        @company_lead = CompanyLead.find(params[:id])
+    end
+
+    def company_lead_params
         params.require(:company_lead).permit(:name, :lastName, :email, :cellphone, :company_lead_source_id)
     end
 end
